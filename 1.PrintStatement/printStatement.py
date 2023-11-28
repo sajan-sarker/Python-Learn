e936cd66968ad07faa44e148d60c29a3cfb91198
Sajan Kumer Sarker <sajansarker47@gmail.com>
2023-11-28 21:29:50 +0600

    .

---
+++ 1.PrintStatement/printStatement.py
@@ -0,0 +1,54 @@
+print("ByE "+"WoRlD "+"!")
+
+print(""""ByE
+      WorlD.""")
+
+print(10+10)
+print(10-10)
+print(10*10)
+print(10/10)
+print(10%8)
+print(30//4)
+
+print("Sum: " + "(10+10)")
+
+name= "John"
+print(name)
+a= 5 
+b= 10
+
+print(a+b)
+
+print("Name is: "+ name)
+age= 23
+print(age)
+cgpa= 3.5
+print(cgpa)
+print("CGPA is: " + str(cgpa))
+print(str(age) + " " + str(cgpa))
+
+x= 10
+y= 20
+age1 = 30
+cgpa1 = 3.6
+name1= "John Snow"
+
+print("Name is: "+ name1)
+print("Age is: "+ str(age1))
+print("CGPA is: "+ str(cgpa1))
+print("Sum is: "+ str(x+y))
+print("X   Name  Age   CGPA")
+print(x, name1, age1, cgpa1)
+
+print(len(name1))
+print(name1.upper())
+print(name1.lower())
+
+arr= [1,2,3,4,5]
+
+print("Index 4: "+str(arr[4]))
+print("Index 0: "+str(arr[0]))
+print("Index 3: "+str(arr[3]))
+
+s= a+b
+print(s)
